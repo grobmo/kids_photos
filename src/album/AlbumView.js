11c9e0f80a132b08e57fe3e9c8c450226092b35c
grobmo <webmaster@grobmo.de>
2020-08-03 23:11:23 +0200

    AlbumView

---
+++ src/album/AlbumView.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+
+export class AlbumView extends React.Component {
+  render() {
+      return (
+          <div>
+              <h1>
+                  jdnsnsdjkfnsdfn
+              </h1>
+              <p>
+                  nnejnhernjhvrenhj
+              </p>
+          </div>
+          
+      );
+  }
+}
